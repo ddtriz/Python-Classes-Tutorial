c6041e3720dbb4a3eb76e8c34cbfc580068ebe3d
Ddtriz <>
2021-05-27 10:46:28 +0000

    Class tutorial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-class firstClass():
+#What is a class?
+
+class firstClass():#Determine a class in the code by using [class]
   name = ""
   identification = 0
   
@@ -9,17 +11,24 @@ firstClass.name = "John"
 firstClass.identification = 326536123
 #If you print the firstClass you'll get something like <class  '__main__.firstClass>
 #That's because you're printing the class and not any attribute of it.
-print(firstClass)
 #You can call them by using the [.] after firstClass
 #if you have a print in your class and you call it
 #(firstClass())
 #it will act like a function. (In this case it will print #hello, as I wrote print("hello") in the class.)
 
-firstClass() #OUTPUT: hello
-print(firstClass)#Output: 
+firstClass() #OUTPUT:[ hello ]
+print(firstClass)#OUTPUT:[ <class '__main__.firstClass> ]
+#Output using Attributes! :D
+print(firstClass.name)#OUTPUT:[ Jhon ]
+print(firstClass.identification)#OUTPUT:[  ]
+
 
 
 
 
 fC = firstClass()
 
+
+
+
+#hope you like it and understood it.
